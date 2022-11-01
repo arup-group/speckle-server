4b86dda1efdec29846e7387ce0aa51e8b9b7af8b
Ops Jenessa Man <Ops.Jenessa.Man@arup.com>
2022-11-01 18:03:27 -0400

    Change argument type in cron helper

--- packages/server/modules/core/services/taskScheduler.ts
+++ packages/server/modules/core/services/taskScheduler.ts
@@ -59,7 +59,7 @@ export const scheduleExecution = (
     throw new InvalidArgumentError(
       `The given cron expression ${cronExpression} is not valid`
     )
-  return cron.schedule(cronExpression, async (scheduledTime: Date) => {
+  return cron.schedule(cronExpression, async (scheduledTime : any) => {
     await scheduledCallbackWrapper(
       scheduledTime,
       taskName,
